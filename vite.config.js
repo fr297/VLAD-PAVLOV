aaa0e8ac62ed59ac7179911cf5695dff927e6199
Aleksandr Frolov <fr.28.09.07@gmail.com>
2025-10-25 23:22:39 +0200

    changes for Pavlov

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  root: ".", // если index.html в корне
+  build: {
+    outDir: "product", // чтобы сборка шла в папку product, как у тебя было
+    emptyOutDir: true,
+  },
+});
